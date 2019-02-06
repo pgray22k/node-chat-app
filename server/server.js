91f5871faea148e1066f137d3aec8e1f99bb784e
Philip <philip.k.gray@gmail.com>
2019-02-06 12:24:43 -0500

    Add connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@
 require('./config/config');
 
 const path = require('path');
+const http = require('http');
 const express = require( 'express');
+const socketIO = require('socket.io');
 //const bodyParser = require('body-parser');
 
 const publicPath = path.join(__dirname, '../public');
@@ -11,7 +13,27 @@ const publicPath = path.join(__dirname, '../public');
 //console.log(publicPath); // --- this should give me the public path directory in a cleaner way
 
 var app = express(); //create routes, middleware, or startup the server
+var server = http.createServer( app );  //http and express are integrated so we just need to pass the app variable
+// var server = http.createServer((request, response) =>{
+//
+// }); //creating server
 
+/*
+creating an io... this is how we are going to communicate between the server and the client
+ */
+var io = socketIO(server);
+
+/*
+    when you call this method it register for  event listener on th server
+    connection - listens when there is a new connection
+ */
+io.on('connection', (socket)=> {
+    console.log( 'New User connected');
+
+    socket.on('disconnect', ()=> {
+       console.log('Client is disconnected');
+    });
+});
 /*
 create middleware using express - middleware gives us the ability to point to other locations to handle html, js, etc.
 __dirname stores the path of your projects directory in our case it is node-web-server
@@ -27,6 +49,6 @@ app.use(express.static( publicPath ));
 //middle ware for handling json for express
 //app.use( bodyParser.json() );
 
-app.listen( process.env.PORT, () => {
+server.listen( process.env.PORT, () => {
     console.log(`Server is up on port ${process.env.port}`);
 });
